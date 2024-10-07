ed4f7db192c7f3d3cfe22c1fb161df4c0c012305
uzzalbhuiyan318 <21303179@iubat.edu>
2024-10-08 01:35:44 +0600

    starting problem

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Test{
+    static void Main(string[] args){
+        int number;
+        Console.WriteLine("Enter your desire number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if(number % 2 == 0){
+            Console.WriteLine("Given Number is even");
+        }
+        else{
+            Console.WriteLine("Given Number is Odd");
+        }
+    }
+}
